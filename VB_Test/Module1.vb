9c4ef91eb9fe04553c49df41c293c1457af4cdff
ThoangRikai <thoang.tran@rikai.technology>
2023-05-09 22:04:45 +0700

    Test VB - git command

---
+++ VB_Test/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim i As Integer = 2
+        While i < 6
+            Console.WriteLine("i value: {0}", i)
+            i += 1
+            Dim j As Integer = 2
+            While j < 4
+                Console.WriteLine("j value: {0}", j)
+                j += 1
+            End While
+        End While
+        Console.WriteLine("Press Enter Key to Exit...")
+        Console.ReadLine()
+    End Sub
+
+End Module
