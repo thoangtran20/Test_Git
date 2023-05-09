496e844450de06b9e5a28aff8caeb864a120836e
ThoangRikai <thoang.tran@rikai.technology>
2023-05-09 22:15:59 +0700

    Update

--- VB_Test/Module1.vb
+++ VB_Test/Module1.vb
@@ -1,18 +1,20 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim i As Integer = 2
-        While i < 6
-            Console.WriteLine("i value: {0}", i)
-            i += 1
-            Dim j As Integer = 2
-            While j < 4
-                Console.WriteLine("j value: {0}", j)
-                j += 1
-            End While
-        End While
-        Console.WriteLine("Press Enter Key to Exit...")
-        Console.ReadLine()
+        'Dim i As Integer = 2
+        'While i < 6
+        '    Console.WriteLine("i value: {0}", i)
+        '    i += 1
+        '    Dim j As Integer = 2
+        '    While j < 4
+        '        Console.WriteLine("j value: {0}", j)
+        '        j += 1
+        '    End While
+        'End While
+        'Console.WriteLine("Press Enter Key to Exit...")
+        'Console.ReadLine()
+
+        'asdfsdsdfadasdffasdfasdfasdfsdf
     End Sub
 
 End Module
